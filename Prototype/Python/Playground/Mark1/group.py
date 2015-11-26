4430569284127bcceb93129f4e7f67eda48d6bc2
cyberewok <cyberewok@users.noreply.github.com>
2015-11-26 17:54:19 +1100

    Schreier generators!
    Finished function for finding Schreier graphs, coset representatives and
    Schreier generators! Next on the hit list is membership testing then the
    Schreier-Sims algorithm should be fairly trivial!

--- Prototype/Python/Playground/Mark1/group.py
+++ Prototype/Python/Playground/Mark1/group.py
@@ -21,23 +21,67 @@ class PermGroup():
             edges = self.generators
         if schreier_graph is None:    
             schreier_graph = [None for _ in range(len(self.identity))]
-            schreier_graph[num] = self.identity
-        frontier = [i for i in schreier_graph if i is not None]
+            schreier_graph[num - 1] = self.identity
+        frontier = [i + 1 for i, g in enumerate(schreier_graph) if g is not None]
         cur_index = 0
         while len(frontier) > cur_index:
             cur_num = frontier[cur_index]
             for g in edges:
                 g_inv = g**-1
                 image = cur_num**g_inv
-                if schreier_graph[image] is None:
-                    schreier_graph[image] = g
+                if schreier_graph[image - 1] is None:
+                    schreier_graph[image - 1] = g
                     frontier.append(image)
             cur_index += 1
         return schreier_graph
+    
+    def _coset_reps(self, schreier_graph):
+        coset_reps = [None for _ in schreier_graph]
+        coset_reps[schreier_graph.index(self.identity)] = self.identity
+        for index in range(len(schreier_graph)):
+            indices_to_coset = [index]
+            while coset_reps[indices_to_coset[-1]] is None:
+                cur_index = indices_to_coset[-1]
+                cur_g = schreier_graph[cur_index]
+                cur_num = (cur_index + 1)
+                image = cur_num**cur_g
+                image_index = image - 1
+                indices_to_coset.append(image_index)
+            prev_index = indices_to_coset.pop()
+            while len(indices_to_coset) > 0:
+                cur_index = indices_to_coset.pop()
+                coset_reps[cur_index] = schreier_graph[cur_index] * coset_reps[prev_index]
+                prev_index = cur_index
+        coset_reps = [g**-1 for g in coset_reps]
+        return coset_reps
+            
+    
+    def _schreier_generators(self, num, coset_reps, edges = None):
+        if edges is None:
+            edges = self.generators
+        schreier_gens = []
+        unique_check = {self.identity}
+        for r in coset_reps:
+            for s in edges:
+                rs = r * s
+                rs_coset_index = (num**rs) - 1
+                rs_coset_rep = coset_reps[rs_coset_index]
+                gen = rs * rs_coset_rep**-1
+                if gen not in unique_check:    
+                    schreier_gens.append(gen)
+                    unique_check.add(gen)
+        return schreier_gens    
 
 def main():
-    s1 = Permutation
-    G = PermGroup()
+    s1 = Permutation.read_cycle_form([[2,3]], 4)
+    s2 = Permutation.read_cycle_form([[1,2,4]], 4)
+    G = PermGroup([s1, s2])
+    s_g = G._schreier_graph(2)
+    print(s_g)
+    cosets = G._coset_reps(s_g)
+    print(cosets)
+    s_gen = G._schreier_generators(2, cosets)
+    print(s_gen)
     
 if __name__ == "__main__":
     main()
--- Prototype/Python/Playground/Mark1/permutation.py
+++ Prototype/Python/Playground/Mark1/permutation.py
@@ -1,15 +1,28 @@
 class Permutation():
-    def __init__(self, nums = None, cycle_form = None, size = None):
-        self._cycle_form = cycle_form
-        if nums == None:
-            
-        self.func=nums
+    def __init__(self, nums):
+        self._func = tuple(nums)
+        self._cycle_form = None
+    
+    @staticmethod
+    def read_cycle_form(cycle_form, size):
+        nums = list(range(1, size + 1))    
+        for cycle in cycle_form:
+            first = cycle[0]
+            last = cycle[-1]
+            nums[last - 1] = first
+            prev = first
+            for num in cycle[1:]:
+                nums[prev - 1] = num
+                prev = num
+        ret = Permutation(nums)
+        ret._cycle_form = cycle_form
+        return ret
     
     def _image(self, num):
         if num < 1 or num > len(self):
             raise ValueError("{} has no image as out of bounds.".format(num))
         else:
-            return self.func[num-1]
+            return self._func[num-1]
     
     def _images(self, nums):
         return [self._image(num) for num in nums]
@@ -17,12 +30,12 @@ class Permutation():
     def __mul__(self, perm):
         if not isinstance(perm, self.__class__):
             raise TypeError("Cannot multiply types {} and {}.".format(type(perm), type(self)))     
-        return Permutation(perm._images(self.func))
+        return Permutation(perm._images(self._func))
     
     def __pow__(self, num):
         if num == -1:
             inverse = [-1 for _ in range(len(self))]
-            for index, value in enumerate(self.func):
+            for index, value in enumerate(self._func):
                 inverse[value - 1] = index + 1
             return Permutation(inverse)
         else:
@@ -40,24 +53,36 @@ class Permutation():
         return str(self)
     
     def __len__(self):
-        return len(self.func)
+        return len(self._func)
+    
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self._func == other._func
+        else:
+            return False
+    
+    def __ne__(self, other):
+        return not self.__eq__(other) 
+    
+    def __hash__(self):
+        return hash(self._func)
         
     def cycle_notation(self):
         if not self._cycle_form is None:
             return self._cycle_form
         ret = []
-        todo = [0 for _ in range(len(self) + 1)]
+        todo = [1 for _ in range(len(self) + 1)]
         for first in range(1, len(todo)):
-            if todo[first] == -1:
+            if todo[first] == 0:
                 continue
             nex = self._image(first)
             if nex != first:
-                todo[first] = -1
-                todo[nex] = -1
+                todo[first] = 0
+                todo[nex] = 0
                 cycle = [first, nex]
                 nex = self._image(nex)
                 while nex != first:
-                    todo[nex] = -1
+                    todo[nex] = 0
                     cycle.append(nex)
                     nex = self._image(nex)
                 ret.append(cycle)
@@ -72,9 +97,10 @@ def main():
     print(a**-1)
     print(a**-1 * a)
     print(a * a**-1)
-    print((a * a**-1).func)
+    print(1 ** a ** -1)
+    print((a * a**-1)._func)
     print(c)
-    print(c.func)
+    print(c._func)
     
 
 if __name__ == "__main__":
