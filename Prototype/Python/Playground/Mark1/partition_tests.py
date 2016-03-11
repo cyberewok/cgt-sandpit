08440584787ca5ffd4396d0b9d00b6e62f3a4da1
cyberewok <cyberewok@users.noreply.github.com>
2016-03-11 17:10:56 +1100

    More partition stabilizer properties done
    Fix extensions and popping all tested and supported.

--- Prototype/Python/Playground/Mark1/partition_tests.py
+++ Prototype/Python/Playground/Mark1/partition_tests.py
@@ -22,9 +22,58 @@ class TestPartition(unittest.TestCase):
         self.assertEqual(b,c)
         
 class TestPartitionStack(unittest.TestCase):
-    def test_init(self):
-        pass
-    
+    def test_use_case(self):
+        stack = PartitionStack([6,2,4,7,0,0,3,5,1,4],[1,1,0,2,-1,-1,0,4,0,0])
+        self.assertEqual(len(stack), 8)
+        self.assertEqual(stack[-1], Partition([[5,6],[9],[2],[7],[3,10],[8],[1],[4]]))
+        self.assertEqual(stack[4][2], [2,4])
+        self.assertEqual(stack[-1][4], [3,10])
+        self.assertEqual(stack[-1][-1], [4])
+
+    def test_get_item(self):
+        a = PartitionStack([0,3,1,2,1],[-1,0,0,1,0])
+        self.assertEqual(a[0], Partition([[1,2,3,4,5]]))
+        self.assertEqual(a[1], Partition([[1,2],[3,4,5]]))
+        self.assertEqual(a[2], Partition([[1,2],[3,5],[4]]))
+        self.assertEqual(a[3], Partition([[1],[3,5],[4],[2]]))
+        self.assertEqual(a[-1], Partition([[1],[3,5],[4],[2]]))
+        
+    def test_extend(self):
+        base = PartitionStack([0,0,0,0,0],[-1,-1,-1,-1,-1])
+        self.assertEqual(base[-1], Partition([[1,2,3,4,5]]))
+        base.extend(0,[3,4,5])
+        self.assertEqual(base[-1], Partition([[1,2],[3,4,5]]))
+        base.extend(1,[4])
+        self.assertEqual(base[-1], Partition([[1,2],[3,5],[4]]))
+        base.extend(0,[2])
+        self.assertEqual(base[-1], Partition([[1],[3,5],[4],[2]]))
+        extention = base.extend(1,[5])
+        self.assertEqual(extention[-1], Partition([[1],[3],[4],[2],[5]]))
+        self.assertEqual(base[-1], Partition([[1],[3],[4],[2],[5]]))
+        
+        self.assertEqual(extention[0], Partition([[1,2,3,4,5]]))
+        self.assertEqual(extention[1], Partition([[1,2],[3,4,5]]))
+        self.assertEqual(extention[2], Partition([[1,2],[3,5],[4]]))
+        self.assertEqual(extention[3], Partition([[1],[3,5],[4],[2]]))
+        self.assertEqual(extention[4], Partition([[1],[3],[4],[2],[5]]))
+        
+    def test_fix(self):
+        stack = PartitionStack([6,2,4,7,0,0,3,5,1,4],[1,1,0,2,-1,-1,0,4,0,0])
+        self.assertEqual(stack.fix(), [7,8,1,9,4,2])
+        
+    def test_pop(self):
+        stack = PartitionStack([6,2,4,7,0,0,3,5,1,4],[1,1,0,2,-1,-1,0,4,0,0])
+        self.assertEqual(stack[-1], Partition([[5,6],[9],[2],[7],[3,10],[8],[1],[4]]))
+        stack.pop()
+        self.assertEqual(stack[-1], Partition([[5,6],[9],[2,4],[7],[3,10],[8],[1]]))
+        self.assertEqual(stack, PartitionStack([6,2,4,2,0,0,3,5,1,4],[1,1,0,1,-1,-1,0,4,0,0]))
+        stack.pop()
+        self.assertEqual(stack[-1], Partition([[5,6],[1,9],[2,4],[7],[3,10],[8]]))
+        stack.pop()
+        self.assertEqual(stack[-1], Partition([[5,6],[1,9],[2,4],[7],[3,8,10]]))
+        stack.pop()
+        self.assertEqual(stack[-1], Partition([[3,5,6,8,10],[1,9],[2,4],[7]]))    
+        
 def all_tests_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestPartition))
